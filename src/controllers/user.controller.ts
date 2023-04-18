fdb792f49669d81153c12d65d6f658a643be00be
Gopi <gopinath10007@gmail.com>
2023-04-18 19:35:10 +0530

    sqlite

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -3,13 +3,19 @@ import { AppDataSource } from '../data-source';
 import { User } from '../entity/User';
 
 export const getUser = async (req: Request, res: Response) => {
-    console.log('iniy');
     const userRepo = AppDataSource.getRepository(User);
     const user = await userRepo.find({
         select: {
             firstName: true,
         },
     });
+    res.status(200).json({ mes: 'ok', data: user });
+    return;
+};
+
+export const createUser = async (req: Request, res: Response) => {
+    const userRepo = AppDataSource.getRepository(User);
+    const user = await userRepo.save(req.body);
     res.status(200).json({ mes: 'ok', data: user });
     return;
 };
