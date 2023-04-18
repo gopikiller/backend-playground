d7968416ec2774cbb8b9eae559991aaadc115f16
Gopi <gopinath10007@gmail.com>
2023-04-18 19:35:57 +0530

    sqlite

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,4 @@
-import { getUser } from './controllers/user.controller';
+import { createUser, getUser } from './controllers/user.controller';
 import { RouteType } from './types/routeType';
 
 const versionPath = `/api/v1.0`;
@@ -7,6 +7,12 @@ export const routes: RouteType[] = [
         method: 'get',
         route: `${versionPath}/user`,
         main: getUser,
+        authRole: ['admin'],
+    },
+    {
+        method: 'post',
+        route: `${versionPath}/user`,
+        main: createUser,
         authRole: ['admin'],
     },
 ];
